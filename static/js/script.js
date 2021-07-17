5d05c1ca07bfd14896ea7f2fa6a5acaa5a3b8c77
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-07-17 17:02:12 +0530

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -63,7 +63,7 @@
       }, 0);
   })();*/
 
-  var deadline = new Date("Jul 16, 2021 10:00:00").getTime(); 
+  var deadline = new Date("Jul 29, 2021 10:00:00").getTime(); 
   
   var x = setInterval(function() { 
     
@@ -85,3 +85,33 @@
           document.getElementById("minute").innerHTML ='0' ;  
           document.getElementById("second").innerHTML = '0'; } 
   }, 1000); 
+
+
+
+
+
+
+
+
+
+
+
+    window.onload = function () {
+        const EFFECT = document.querySelector("#effect");
+        window.addEventListener('scroll', scrollEffect);
+        function scrollEffect() {
+            if (window.scrollY >= 500) {
+                EFFECT.style.opacity = '1';
+                EFFECT.style.transform = 'translateX(0px)';
+                EFFECT.style.transition = '1s ease-in-out';
+            }
+            else {
+                EFFECT.style.opacity = '0';
+                EFFECT.style.transform = 'translateX(-50px)';
+
+            }
+        }
+        scrollEffect();
+    }
+
+
