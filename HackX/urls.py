b1b586e8905852141823ab21fbd72b73e026d747
sunny-1651 <55059942+sunny-1651@users.noreply.github.com>
2021-07-17 17:47:49 +0530

    Update urls.py

--- HackX/urls.py
+++ HackX/urls.py
@@ -20,3 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hackxsps.urls'))
 ]
+
+url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
